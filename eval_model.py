c48336d8da3a699f71cadc9f5e764fd7b1746fcd
artfin <artfin@mail.ru>
2022-01-31 14:43:17 +0300

    evaluating model on the whole test set

--- eval_model.py
+++ eval_model.py
@@ -9,6 +9,10 @@ from model import FCNet
 from dataset import PolyDataset
 from dataset import HTOCM
 
+# very important to fix the seed !
+torch.manual_seed(42)
+np.random.seed(42)
+
 if __name__ == '__main__':
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
@@ -40,17 +44,34 @@ if __name__ == '__main__':
     wdir     = './H2-H2O'
     order    = "3"
     symmetry = "2 2 1"
-    dataset  = PolyDataset(wdir=wdir, config_fname='test.dat', order=order, symmetry=symmetry)
+    dataset  = PolyDataset(wdir=wdir, config_fname='points.dat', order=order, symmetry=symmetry)
 
-    mean = np.loadtxt("_mean.txt")
-    std  = np.loadtxt("_std.txt")
+    ids = np.random.permutation(len(dataset))
+    train_size = int(0.8 * len(dataset))
+    test_size  = len(dataset) - train_size
+    logging.info("Test size  = {}".format(test_size))
 
-    X, y = dataset.X, dataset.y
-    X = (X - mean) / std
+    train_ids = ids[:train_size]
+    test_ids  = ids[train_size:]
 
-    print(X)
+    test_ids         = ids[train_size:]
+    X, y             = dataset.X, dataset.y
+    X_train, y_train = X[train_ids], y[train_ids]
+    X_test, y_test   = X[test_ids], y[test_ids]
 
-    y_pred = model(X)
+    # scaling
+    mean = X_train.mean(axis=0)
+    std  = X_train.std(axis=0)
+    # first polynomial is a constant => std = 0.0
+    std[0] = 1.0
 
-    print("y_pred = {0:.8f}".format(y_pred.item() * HTOCM))
-    print("y      = {0:.8f}".format(y.item() * HTOCM))
+    X_test = (X_test - mean) / std
+
+    y_pred = model(X_test)
+    y_pred = y_pred.detach().numpy()
+
+
+    print("Prediction \t Test \t Diff")
+    for ypred_val, ytest_val in zip(y_pred, y_test):
+        diff = ypred_val[0] - ytest_val[0]
+        print(" {:.8f} \t {:.8f} \t {:.8f}".format(ypred_val[0] * HTOCM, ytest_val[0] * HTOCM, diff * HTOCM))
