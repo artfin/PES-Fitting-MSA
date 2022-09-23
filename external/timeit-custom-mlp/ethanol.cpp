75fecbdc40326c5de7232e32212f129a693578d9
artfin <artfin@mail.ru>
2022-09-23 18:13:38 -0400

    omp performace check of the ethanol model runtime

--- external/timeit-custom-mlp/ethanol.cpp
+++ external/timeit-custom-mlp/ethanol.cpp
@@ -1,3 +1,6 @@
+#include <omp.h>
+#include <unistd.h>
+
 #include <iostream>
 #include <iomanip>
 #include <chrono>
@@ -36,6 +39,58 @@ void make_yij_1_1_1_2_3_1(const double * x, double* yij, int natoms)
 #define EVPOLY_JAC evpoly_jac_1_1_1_2_3_1_3
 #define MAKE_YIJ   make_yij_1_1_1_2_3_1
 #include "mlp.hpp"
+
+void time_ethanol_energy_and_forces_omp() {
+
+    const int maxNumThreads = omp_get_max_threads();
+    omp_set_num_threads(maxNumThreads);
+
+    int np = 20000;
+    double st = omp_get_wtime();
+    
+    #pragma omp parallel
+    {
+        auto model = build_model_from_npz("models/ethanol-wf-32-ntrain=50000.npz");
+    
+        std::random_device rd;
+        std::mt19937 mt(rd()); 
+        std::uniform_real_distribution<> dist(0, 1); 
+
+        std::vector<double> c = { 
+            -0.48656453, -0.89968471,  0.33532281,
+            -1.75840118,  1.72792291,  0.23343237,
+             2.06343414, -0.81795535, -0.44480756,
+            -0.62151551, -1.24660767,  2.59927346,
+            -1.42294266, -2.532772  , -0.64166572,
+            -3.71060312,  1.32700928,  1.02877824,
+            -0.62999519,  3.16756379,  1.12270384,
+            -2.01849358,  2.04032063, -1.81168631,
+             2.39924183,  0.35599741, -2.01414636
+        };
+
+        int natoms = 9;
+        std::vector<double> forces(3 * natoms);
+
+        #pragma omp for 
+        for (int i = 0; i < np; i++) {
+            int tid = omp_get_thread_num();
+            
+            for (size_t natom = 0; natom < natoms; ++natom) {
+                c[natom] += 0.001 * dist(mt);
+            }
+
+            double en = model.forward(c);
+            model.backward(c, forces);
+            //std::cout << "tid: " << tid << "; en: " << en << std::endl;
+        }
+    }
+
+    double end = omp_get_wtime();
+
+    double elapsed = end - st;
+    double per_call = elapsed / np * 1e6;
+    std::cout << "Elapsed " << elapsed << "s; per call: " <<  per_call  << " mcs \n";
+}
 
 void time_ethanol_forces() {
     auto model = build_model_from_npz("models/ethanol-32.npz");
@@ -194,9 +249,11 @@ void check_ethanol_model()
 
 int main()
 {
-    check_ethanol_model();
-    time_ethanol_energy();
-    time_ethanol_forces();
+    time_ethanol_energy_and_forces_omp();
+
+    //check_ethanol_model();
+    //time_ethanol_energy();
+    //time_ethanol_forces();
 
     return 0;
 }
